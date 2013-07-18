beec623b50575cfb9b34a0f860ed8366957f3208
Lou Markovski <lou.markovski@gmail.com>
2013-07-18 14:23:03 +1000

    task/ta33 -- flag ODBC connection failures
    bin/nparceld.py
    * connection check prior to each file-based transaction
    
    dbsession.py
    * __call__ method now doubles as a connection check
    * __call__ will attempt to clean up the connection on a
      pyodbc.ProgrammingError

--- bin/nparceld.py
+++ bin/nparceld.py
@@ -20,24 +20,28 @@ def main():
     file = 'T1250_TOLP_20130413135756.txt'
 
     log.info('Processing file: "%s" ...' % file)
-    try:
-        f = open(file, 'r')
 
-        reporter.reset()
-        loader.reset()
-        for line in f:
-            record = line.rstrip('\r\n')
-            if record == '%%EOF':
-                log.info('EOF found')
-                reporter.end()
-                reporter.set_failed_log(loader.alerts)
-                reporter.report()
-            else:
-                reporter(loader.process(record))
+    if loader.db():
+        try:
+            f = open(file, 'r')
 
-        f.close()
-    except IOError, e:
-        log.error('Error opening file "%s": %s' % (file, str(e)))
+            reporter.reset()
+            loader.reset()
+            for line in f:
+                record = line.rstrip('\r\n')
+                if record == '%%EOF':
+                    log.info('EOF found')
+                    reporter.end()
+                    reporter.set_failed_log(loader.alerts)
+                    reporter.report()
+                else:
+                    reporter(loader.process(record))
+
+            f.close()
+        except IOError, e:
+            log.error('Error opening file "%s": %s' % (file, str(e)))
+    else:
+        log.error('ODBC connection failure -- aborting')
 
 if __name__ == '__main__':
     main()
