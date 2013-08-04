9fce58129de247be7a729e1ff84f3852dc575af5
Lou Markovski <lou.markovski@gmail.com>
2013-08-04 20:51:17 +1000

    task task/ta22-14 -- ExporterDaemon() class _start() method hook
    nparcel/exporterdaemon.py
    * new method _start() to commence processing
    * new method _exit_handler() to handle SIGTERM

--- nparcel/exporterdaemon.py
+++ nparcel/exporterdaemon.py
@@ -1,10 +1,30 @@
 __all__ = [
     "ExporterDaemon",
 ]
+import signal
+
 import nparcel
+from nparcel.utils.log import log
+
 
 class ExporterDaemon(nparcel.utils.Daemon):
 
     def __init__(self,
-                 pidfile):
+                 pidfile,
+                 config='nparcel.conf'):
         super(ExporterDaemon, self).__init__(pidfile=pidfile)
+
+        self.config = nparcel.Config(file=config)
+
+    def _start(self, event):
+        signal.signal(signal.SIGTERM, self._exit_handler)
+
+        exporter = nparcel.Exporter(db=self.config.db_kwargs())
+
+        while not event.isSet():
+            event.set()
+
+    def _exit_handler(self, signal, frame):
+        log_msg = '%s --' % type(self).__name__
+        log.info('%s SIGTERM intercepted' % log_msg)
+        self.set_exit_event()
--- nparcel/tests/test_exporterdaemon.py
+++ nparcel/tests/test_exporterdaemon.py
@@ -7,15 +7,18 @@ class TestExporterDaemon(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        cls._ed = nparcel.ExporterDaemon(pidfile=None,
+                                         config='nparcel/conf/nparceld.conf')
 
     def test_init(self):
         """Intialise a ExporterDaemon object.
         """
-        d = nparcel.ExporterDaemon(pidfile=None)
         msg = 'Not a nparcel.ExporterDaemon object'
-        self.assertIsInstance(d, nparcel.ExporterDaemon, msg)
+        self.assertIsInstance(self._ed, nparcel.ExporterDaemon, msg)
+
+    def test_start(self):
+        self._ed._start(self._ed.exit_event)
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        cls._ed = None
