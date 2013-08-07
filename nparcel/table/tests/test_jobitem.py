32860a5c4627d950d4c44db15cb57b1bf0da2b42
Lou Markovski <lou.markovski@gmail.com>
2013-08-07 21:36:21 +1000

    task/ta35-02 -- job_item connote search
    nparcel/table/jobitem.py
    * new method connote_sql() as an SQL wrapper to extract records
      against job_item.connote_nbr

--- nparcel/table/tests/test_jobitem.py
+++ nparcel/table/tests/test_jobitem.py
@@ -12,6 +12,12 @@ class TestJobItem(unittest2.TestCase):
         cls._db = nparcel.DbSession()
         cls._db.connect()
 
+    def test_init(self):
+        """Initialise a JobItem object.
+        """
+        msg = "Object is not an nparcel.JobItem"
+        self.assertIsInstance(self._job_item, nparcel.JobItem, msg)
+
     def test_jobitem_table_insert_with_valid_nparcel_data(self):
         """Insert valid Nparcel data into "job_item" table.
         """
@@ -19,12 +25,37 @@ class TestJobItem(unittest2.TestCase):
                   'consumer_name': 'Diane Donohoe',
                   'pieces': '00001',
                   'status': 1,
-                  'created_ts': datetime.datetime.now().isoformat()}
+                  'created_ts': datetime.datetime.now().isoformat(' ')[:-3]}
         received = self._db.insert(self._job_item.insert_sql(kwargs))
         expected = 1
         msg = '"job_item" table insert returned unexpected row_id'
         self.assertEqual(received, expected, msg)
 
+        self._db.connection.rollback()
+
+    def test_connote_sql(self):
+        """Verify the connote_sql string.
+        """
+        connote = '218501217863'
+
+        # Seed the table with sample data.
+        kwargs = {'connote_nbr': connote,
+                  'status': 1,
+                  'created_ts': datetime.datetime.now().isoformat(' ')[:-3]}
+        self._db(self._job_item.insert_sql(kwargs))
+
+        # Check the query via SQL.
+        sql = self._db.jobitem.connote_sql(connote=connote)
+        self._db(sql)
+
+        received = []
+        for row in self._db.rows():
+            received.append(row[0])
+        expected = [1]
+        msg = 'job_item.connote return value not as expected'
+        self.assertListEqual(received, expected, msg)
+
+        # Cleanup.
         self._db.connection.rollback()
 
     @classmethod
