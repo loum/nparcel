77fd9959678978359e5e10e269ccdb2e2aa9fd4c
Lou Markovski <lou.markovski@gmail.com>
2013-08-09 12:52:04 +1000

    task/ta22-23 -- integrate config
    nparcel/conf/npftp.conf
    * sample config file used for testing
    
    nparcel/ftp.py
    * new methods set_config_file(), _parse_config() to manage the
      configuration items

--- nparcel/ftp.py
+++ nparcel/ftp.py
@@ -1,15 +1,61 @@
 __all__ = [
     "Ftp",
 ]
+import os
+import sys
 import ftplib
-import nparcel
+import ConfigParser
+
+from nparcel.utils.log import log
 
 
 class Ftp(ftplib.FTP):
     """Nparcel FTP client.
     """
 
-    def __init__(self):
+    def __init__(self,
+                 config_file='npftp.conf'):
         """Nparcel Ftp initialisation.
         """
         ftplib.FTP.__init__(self)
+
+        self._config = ConfigParser.SafeConfigParser()
+        self._xfers = []
+
+        self.set_config_file(config_file)
+
+    @property
+    def xfers(self):
+        return self._xfers
+
+    @property
+    def config_file(self):
+        return self._config_file
+
+    def set_config_file(self, value):
+        self._config_file = value
+
+        if self._config_file is not None:
+            if os.path.exists(self._config_file):
+                log.info('Parsing config file: "%s"' % self._config_file)
+                self._config.read(self._config_file)
+            else:
+                log.critical('Unable to locate config file: "%s"' %
+                             self._config_file)
+                sys.exit(1)
+
+    def _parse_config(self):
+        """Read config items from the configuration file.
+
+        Each section that starts with ``ftp_`` are interpreted as an FTP
+        connection to process.
+
+        """
+        if self.config_file is None:
+            log.critical('Cannot parse config -- no file defined')
+            sys.exit(1)
+
+        # Parse each section.
+        for section in self._config.sections():
+            if section[:4] == 'ftp_':
+                self._xfers.append(section)
--- nparcel/tests/test_ftp.py
+++ nparcel/tests/test_ftp.py
@@ -7,13 +7,35 @@ class TestFtp(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls._ftp = nparcel.Ftp()
+        cls._ftp = nparcel.Ftp(config_file='nparcel/conf/npftp.conf')
 
     def test_init(self):
         """Initialise an FTP object.
         """
         msg = 'Object is not an nparcel.Ftp'
         self.assertIsInstance(self._ftp, nparcel.Ftp, msg)
+
+    def test_parse_config_no_file(self):
+        """Parse config with no file provided.
+        """
+        old_config = self._ftp.config_file
+        self._ftp.set_config_file(None)
+
+        self.assertRaises(SystemExit, self._ftp._parse_config)
+
+        # Cleanup.
+        self._ftp.set_config_file(old_config)
+
+    def test_parse_config_items(self):
+        """Verify required configuration items.
+        """
+        self._ftp._parse_config()
+
+        # Check against expected config items.
+        msg = 'Transfer sections not as expected'
+        received = self._ftp.xfers
+        expected = ['ftp_priority', 'ftp_other']
+        self.assertListEqual(sorted(received), sorted(expected), msg)
 
     @classmethod
     def tearDownClass(cls):
