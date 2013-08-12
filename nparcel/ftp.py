f84537a23318b398b16fb825dd881df7f99655c4
Lou Markovski <lou.markovski@gmail.com>
2013-08-12 18:46:24 +1000

    task/ta22-26 -- FTP identifies report file and generates list of signature files

--- nparcel/ftp.py
+++ nparcel/ftp.py
@@ -4,6 +4,7 @@ __all__ = [
 import os
 import sys
 import ftplib
+import fnmatch
 import ConfigParser
 
 from nparcel.utils.log import log
@@ -23,6 +24,10 @@ class Ftp(ftplib.FTP):
         self._xfers = []
 
         self.set_config_file(config_file)
+
+    @property
+    def config(self):
+        return self._config
 
     @property
     def xfers(self):
@@ -59,3 +64,87 @@ class Ftp(ftplib.FTP):
         for section in self._config.sections():
             if section[:4] == 'ftp_':
                 self._xfers.append(section)
+
+    def get_report_file(self, dir):
+        """Identifies report files in directory *dir*.
+
+        **Args:**
+            dir: the directory to search for report files.
+
+        **Returns:**
+            list of report files
+        """
+        log.info('Looking for report files in directory: "%s"' % dir)
+        for file in os.listdir(dir):
+            file = os.path.join(dir, file)
+            if os.path.isfile(file):
+                if fnmatch.fnmatch(os.path.basename(file),
+                                   'VIC_VANA_RE*.txt'):
+                    log.info('Found report file: "%s"' % file)
+                    yield file
+
+    def get_report_file_ids(self, file):
+        """Parse report file and extract a list of JOB_KEY's
+
+        **Args:**
+            file: fully qualified name to the report file
+
+        **Returns:**
+            list of JOB_KEY values
+
+        """
+        log.info('Checking file "%s" for JOB_KEYs' % file)
+        keys = []
+
+        try:
+            fh = open(file)
+            # Consume the header.
+            fh.readline()
+            for line in fh:
+                keys.append(line.rsplit('|')[1])
+        except IOError, err:
+            log.error('Could not open file "%s"' % file)
+
+        return keys
+
+    def process(self, dry=False):
+        """
+        """
+        self._parse_config()
+        for xfer in self.xfers:
+            files_to_xfer = []
+
+            log.info('Processing transfers for "%s"' % xfer)
+            source = self.config.get(xfer, 'source')
+            log.debug('Sourcing files from local directory: "%s"' % source)
+
+            keys = []
+            for report in self.get_report_file(source):
+                keys = self.get_report_file_ids(report)
+
+                for key in keys:
+                    sig_file = os.path.join(source, '%s.ps' % key)
+                    files_to_xfer.append(sig_file)
+
+                # ... and append the report file.
+                files_to_xfer.append(report)
+
+            if files_to_xfer:
+                self.xfer_files(xfer, files_to_xfer)
+
+    def xfer_files(self, xfer, files):
+        """
+        """
+        host = self.config.get(xfer, 'host')
+        port = self.config.get(xfer, 'port')
+        user = self.config.get(xfer, 'user')
+        password = self.config.get(xfer, 'password')
+        target = self.config.get(xfer, 'target')
+
+        self.connect(host=host, port=port)
+        self.login(user=user, passwd=password)
+
+        for file in files:
+            log.info('Transferring "%s"' % file)
+
+        self.quit()
--- nparcel/tests/test_ftp.py
+++ nparcel/tests/test_ftp.py
@@ -1,4 +1,6 @@
 import unittest2
+import os
+import tempfile
 
 import nparcel
 
@@ -8,6 +10,9 @@ class TestFtp(unittest2.TestCase):
     @classmethod
     def setUpClass(cls):
         cls._ftp = nparcel.Ftp(config_file='nparcel/conf/npftp.conf')
+
+        # Create a temporary directory structure.
+        cls._dir = tempfile.mkdtemp()
 
     def test_init(self):
         """Initialise an FTP object.
@@ -37,6 +42,53 @@ class TestFtp(unittest2.TestCase):
         expected = ['ftp_priority', 'ftp_other']
         self.assertListEqual(sorted(received), sorted(expected), msg)
 
+    def test_get_report_file_no_files(self):
+        """Check directory for report files -- no files defined.
+        """
+        received = []
+        for file in self._ftp.get_report_file(self._dir):
+            received.append(file)
+        msg = 'No reports should be found in an empty directory'
+        self.assertListEqual(received, [], msg)
+
+    def test_get_report_file(self):
+        """Check directory for report files -- valid file defined.
+        """
+        # Fudge a report file name.
+        report = os.path.join(self._dir, 'VIC_VANA_REP_20130812140736.txt')
+        fh = open(report, 'w')
+        fh.close()
+
+        received = []
+        for file in self._ftp.get_report_file(self._dir):
+            received.append(file)
+        expected = [report]
+        msg = 'Report file should be found in directory'
+        self.assertListEqual(received, expected, msg)
+
+        # Cleanup.
+        os.remove(report)
+
+    def test_get_report_file(self):
+        """Check directory for report files -- valid file defined.
+        """
+        # Fudge a report file name.
+        report = os.path.join(self._dir, 'VIC_VANA_REP_20130812140736.txt')
+        fh = open(report, 'w')
+        fh.write('REF1|JOB_KEY|PICKUP_TIME|PICKUP_POD|IDENTITY_TYPE|IDENTITY_DATA\n')
+        fh.write('ALHZ104346|5|2013-02-01 01:23:45|UNCOLLECTED PARCEL DATACLEANUP|Other|0000')
+        fh.close()
+
+        received = self._ftp.get_report_file_ids(report)
+        expected = ['5']
+        msg = 'Report file should be found in directory'
+        self.assertListEqual(received, expected, msg)
+
+        # Cleanup.
+        os.remove(report)
+
     @classmethod
     def tearDownClass(cls):
         cls._ftp = None
+        os.removedirs(cls._dir)
+        cls._dir = None
