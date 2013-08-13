b7ccbaba421da65fdd7745e392205b2d2d635fda
Lou Markovski <lou.markovski@gmail.com>
2013-08-13 12:37:29 +1000

    Roll over to version 0.5

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name='python-nparcel',
-      version='0.4',
+      version='0.5',
       description='Nparcel B2C Replicator',
       author='Lou Markovski',
       author_email='lou.markovski@tollgroup.com',
       url='https://nparcel.tollgroup.com',
       scripts=['nparcel/bin/nploaderd',
-               'nparcel/bin/npexporterd'],
+               'nparcel/bin/npexporterd',
+               'nparcel/bin/npftp'],
       packages=['nparcel', 'nparcel.table', 'nparcel.utils'],)
