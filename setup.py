6a6786a4247fab388cda3412868548f5313ba416
Lou Markovski <lou.markovski@gmail.com>
2013-08-15 15:57:43 +1000

    Prepare for release 0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='python-nparcel',
-      version='0.5',
+      version='0.6',
       description='Nparcel B2C Replicator',
       author='Lou Markovski',
       author_email='lou.markovski@tollgroup.com',
