204a4f25b50274a53fa5fb398404f0c8f3ea5ff2
Lou Markovski <lou.markovski@gmail.com>
2013-10-17 13:45:26 +1100

    task/ta-npe-load-02 -- db configuration
    nparcel/mts.py
    * parse DB connection string and present via new property, conn_string

--- nparcel/mts.py
+++ nparcel/mts.py
@@ -1,7 +1,10 @@
 __all__ = [
      "Mts",
 ]
+import ConfigParser
+
 import nparcel
+from nparcel.utils.log import log
 
 
 class Mts(nparcel.Service):
@@ -18,3 +21,64 @@ class Mts(nparcel.Service):
         db = None
 
         super(nparcel.Mts, self).__init__(db=db)
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def conn_string(self):
+        db_kwargs = self._db_kwargs()
+
+        host = db_kwargs.get('host')
+        user = db_kwargs.get('user')
+        password = db_kwargs.get('password')
+        port = db_kwargs.get('port')
+        sid = db_kwargs.get('sid')
+
+        return '%s/%s@%s:%d/%s' % (user, password, host, port, sid)
+
+    def _db_kwargs(self):
+        """Extract database connectivity information from the configuration.
+
+        Database connectivity information is taken from the ``[db]``
+        section in the configuration file.  A typical example is::
+
+            [db]
+            host = tsspodb2
+            user = TOLL_DW_MTS
+            password = <password>
+            port =  1521
+            database = GC3DW
+
+        **Returns:**
+            dictionary-based data structure of the form::
+
+                kwargs = {'host': ...,
+                          'user': ...,
+                          'password': ...,
+                          'port': ...,
+                          'database': ...}
+
+        """
+        db_kwargs = None
+
+        try:
+            host = self.config.get('db', 'host')
+            user = self.config.get('db', 'user')
+            password = self.config.get('db', 'password')
+            port = self.config.get('db', 'port')
+            sid = self.config.get('db', 'sid')
+
+            if port is not None:
+                port = int(port)
+            db_kwargs = {'host': host,
+                         'user': user,
+                         'password': password,
+                         'port': port,
+                         'sid': sid}
+        except (ConfigParser.NoSectionError,
+                ConfigParser.NoOptionError), err:
+            log.info('Missing DB key via config: %s' % err)
+
+        return db_kwargs
--- nparcel/tests/test_mts.py
+++ nparcel/tests/test_mts.py
@@ -15,6 +15,26 @@ class TestMts(unittest2.TestCase):
         msg = 'Object is not an nparcel.Mts'
         self.assertIsInstance(self._mts, nparcel.Mts, msg)
 
+    def test_db_kwargs(self):
+        """Verify the DB kwargs.
+        """
+        received = self._mts._db_kwargs()
+        expected = {'host': 'host',
+                    'user': 'user',
+                    'password': 'password',
+                    'port': 1521,
+                    'sid': 'sid'}
+        msg = 'Database kwargs error'
+        self.assertDictEqual(received, expected, msg)
+
+    def test_db_connection_string(self):
+        """DB connection string.
+        """
+        received = self._mts.conn_string
+        expected = 'user/password@host:1521/sid'
+        msg = 'DB connection string error'
+        self.assertEqual(received, expected, msg)
+
     @classmethod
     def tearUpClass(cls):
         cls._mts = None
