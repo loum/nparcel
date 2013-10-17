5f60eb7594e54f478958942067bb5336b5f92a15
Lou Markovski <lou.markovski@gmail.com>
2013-10-17 16:19:15 +1100

    task/ta-npe-load-05 -- SQL template
    nparcel/mts.py
    * new method get_sql() which generates a generic SQL from a given
      template
    * new method get_delivery_report_sql() which generates the MTS
      delivery report SQL

--- nparcel/mts.py
+++ nparcel/mts.py
@@ -3,6 +3,9 @@ __all__ = [
 ]
 import ConfigParser
 import cx_Oracle
+import string
+import os
+import datetime
 
 import nparcel
 from nparcel.utils.log import log
@@ -117,3 +120,66 @@ class Mts(object):
 
         for row in self._cursor:
             print('Oracle DB Version: %s' % row)
+
+    def get_sql(self, template='mts_sql.t', base_dir=None, **kwargs):
+        """Create the SQL query.
+
+        **Args:**
+            :122
+
+        **Kwargs:**
+            base_dir: override the standard location to search for the
+            SQL template (default ``~user_home/.nparceld/templates``).
+
+            *kwargs*: varargs expected by the template
+
+        """
+        dir = None
+        if base_dir is None:
+            dir = os.path.join(os.path.expanduser('~'),
+                               '.nparceld',
+                               'templates')
+        else:
+            dir = os.path.join(base_dir, 'templates')
+
+        query = None
+        query_file = os.path.join(dir, template)
+        log.debug('Extracting SQL from template: "%s"' % query_file)
+        f = None
+        try:
+            f = open(query_file)
+        except IOError, err:
+            log.error('Unable to open SQL template "%s": %s' %
+                      (query_file, err))
+
+        if f is not None:
+            query_t = f.read()
+            f.close()
+            query_s = string.Template(query_t)
+            query = query_s.substitute(**kwargs)
+
+        return query
+
+    def get_delivery_report_sql(self, base_dir=None):
+        """
+
+        **Kwargs:**
+            base_dir: override the standard location to search for the
+
+        """
+        to_dt = datetime.datetime.now()
+        from_dt = to_dt - datetime.timedelta(seconds=86400)
+
+        to_date = to_dt.strftime('%d/%m/%Y')
+        from_date = from_dt.strftime('%d/%m/%Y')
+
+        sql = None
+
+        sql = self.get_sql('mts_sql.t',
+                           base_dir=base_dir,
+                           to_date=to_date,
+                           from_date=from_date)
+
+        log.debug('SQL: %s' % sql)
+
+        return sql
