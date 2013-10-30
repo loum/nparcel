9347a01ad895e84c4e955117ab9cd75b13fc11b4
Lou Markovski <lou.markovski@gmail.com>
2013-10-30 13:16:27 +1100

    task/ta-npp-002-02 -- filter processing should identify ParcelPoint records
    nparcel/filter.py
    * new method process() which identifies ParcelPoint records

--- nparcel/filter.py
+++ nparcel/filter.py
@@ -18,3 +18,40 @@ class Filter(object):
     @property
     def parser(self):
         return self._parser
+
+    def process(self, raw_record):
+        """Identifies ParcelPoint records.
+
+        Valid ParcelPoint records are identified by an *Agent Id*
+        that starts with ``P``.
+
+        **Args:**
+            *raw_record*: raw record directly from a T1250 file
+
+        *Returns:**
+            boolean ``True`` if record is a ParcelPoint
+
+            boolean ``False`` if record is not a ParcelPoint
+
+            ``None`` if an *other* scenario (typically missing Agent Id)
+
+        """
+        status = None
+
+        connote_literal = raw_record[0:20].rstrip()
+        log.info('Conn Note: "%s" start parse ...' % connote_literal)
+        fields = self.parser.parse_line(raw_record)
+        agent_id = fields.get('Agent Id')
+        msg = 'Conn Note/Agent Id "%s/%s"' % (connote_literal, agent_id)
+        if agent_id is None or not agent_id:
+            log.info('%s - missing Agent Id' % msg)
+        elif agent_id.startswith('P'):
+            log.info('%s - matches criteria' % msg)
+            status = True
+        else:
+            log.info('%s - does not match criteria' % msg)
+            status = False
+
+        log.info('Conn Note: "%s" parse complete' % connote_literal)
+
+        return status
--- nparcel/tests/test_filter.py
+++ nparcel/tests/test_filter.py
@@ -7,6 +7,8 @@ class TestFilter(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        test_conf = 'nparcel/tests/test_loader.conf'
+        cls._c = nparcel.Config(config_file=test_conf)
         cls._f = nparcel.Filter()
 
     def test_init(self):
@@ -15,7 +17,33 @@ class TestFilter(unittest2.TestCase):
         msg = 'Object is not an nparcel.Filter'
         self.assertIsInstance(self._f, nparcel.Filter, msg)
 
+    def test_process_valid_record_parcel_point(self):
+        """Process valid record.
+        """
+        line = self._c.get('test_lines', 'PP')
+        received = self._f.process(line)
+        msg = 'Valid ParcelPoint record should return True'
+        self.assertTrue(received, msg)
+
+    def test_process_missing_agent_id(self):
+        """Process valid record -- missing Agent Id.
+        """
+        line = self._c.get('test_lines', 'PP_MISSING_AGENT_ID')
+        received = self._f.process(line)
+        msg = 'Valid ParcelPoint -- missing Agent Id should return None'
+        self.assertIsNone(received, msg)
+
+    def test_process_valid_record_not_parcel_point(self):
+        """Process valid record - not parcel point.
+        """
+        line = self._c.get('test_lines', 'VALID_LINE')
+        received = self._f.process(line)
+        msg = 'Valid ParcelPoint -- not ParcelPoint should return False'
+        self.assertFalse(received, msg)
+
     @classmethod
     def tearDownClass(cls):
+        cls._c = None
+        del cls._c
         cls._f = None
         del cls._f
