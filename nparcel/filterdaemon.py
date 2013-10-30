baf9ffa9a0efc7966d4b3d42df0f53c6b5061440
Lou Markovski <lou.markovski@gmail.com>
2013-10-30 14:33:17 +1100

    task/ta-npp-002-05 -- introduce daemoniser _start() hook
    nparcel/filterdaemon.py
    * new daemoniser hook method _start()

--- nparcel/filterdaemon.py
+++ nparcel/filterdaemon.py
@@ -1,13 +1,23 @@
 __all__ = [
     "FilterDaemon",
 ]
+import signal
+import time
+
 import nparcel
+from nparcel.utils.log import log
 
 
 class FilterDaemon(nparcel.DaemonService):
     """Daemoniser facility for the :class:`nparcel.Filter` class.
 
+    .. attribute:: file_format
+
+        the :mod:`re` format string to match filter files against
+
     """
+    _file_format = 'T1250_TOL*.txt'
+
     def __init__(self,
                  pidfile,
                  file=None,
@@ -21,3 +31,45 @@ class FilterDaemon(nparcel.DaemonService):
 
         self.config = nparcel.B2CConfig(file=config)
         self.config.parse_config()
+
+    def _start(self, event):
+        """Override the :method:`nparcel.utils.Daemon._start` method.
+
+        Will perform a single iteration if the :attr:`file` attribute has
+        a list of filenames to process.  Similarly, dry and batch modes
+        only cycle through a single iteration.
+
+        **Args:**
+            *event* (:mod:`threading.Event`): Internal semaphore that
+            can be set via the :mod:`signal.signal.SIGTERM` signal event
+            to perform a function within the running proess.
+
+        """
+        signal.signal(signal.SIGTERM, self._exit_handler)
+
+        filter = nparcel.Filter()
+
+        commit = True
+        if self.dry:
+            commit = False
+
+        while not event.isSet():
+            files = []
+            if self.file is not None:
+                files.append(self.file)
+                event.set()
+            else:
+                files.extend(self.get_files())
+
+            if not event.isSet():
+                if self.dry:
+                    log.info('Dry run iteration complete -- aborting')
+                    event.set()
+                elif self.batch:
+                    log.info('Batch run iteration complete -- aborting')
+                    event.set()
+                else:
+                    time.sleep(self.config.filter_loop)
+
+    def get_files(self):
+        return []
--- nparcel/tests/test_filterdaemon.py
+++ nparcel/tests/test_filterdaemon.py
@@ -1,4 +1,5 @@
 import unittest2
+import threading
 
 import nparcel
 
@@ -7,6 +8,8 @@ class TestFilterDaemon(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls._file = 'nparcel/tests/files/T1250_TOLI_20130828202901.txt'
+        cls._exit_event = threading.Event()
         cls._fd = nparcel.FilterDaemon(pidfile=None,
                                        config='nparcel/conf/nparceld.conf')
 
@@ -16,7 +19,26 @@ class TestFilterDaemon(unittest2.TestCase):
         msg = 'Not a nparcel.FilterDaemon object'
         self.assertIsInstance(self._fd, nparcel.FilterDaemon, msg)
 
+    def test_start(self):
+        """Start dry loop.
+        """
+        # Note: were not testing behaviour here but check that we have
+        # one of each, success/error/other.
+        old_file = self._fd.file
+        old_dry = self._fd.dry
+
+        self._fd.set_dry()
+        self._fd.set_file(self._file)
+        self._fd._start(self._exit_event)
+
+        # Clean up.
+        self._fd.set_file(old_file)
+        self._fd.set_dry(old_dry)
+        self._exit_event.clear()
+
     @classmethod
     def tearDownClass(cls):
+        del cls._file
+        del cls._exit_event
         cls._fd = None
         del cls._fd
