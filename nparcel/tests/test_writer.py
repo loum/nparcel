fb505e253e92d24da71c01609004c728fbdb8f3a
Lou Markovski <lou.markovski@gmail.com>
2013-12-03 16:25:49 +1100

    task/ta-nst-011-02 -- write out to a CSV file
    nparcel/writer.py
    * new attributes "headers" and "write_headers"
    * new class callable __call__ that prepares a DictWriter object and
      write out data to a CSV file

--- nparcel/tests/test_writer.py
+++ nparcel/tests/test_writer.py
@@ -1,13 +1,20 @@
 import unittest2
+import tempfile
+import datetime
+import os
 
 import nparcel
+from nparcel.utils.files import remove_files
 
 
 class TestWriter(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls._now = datetime.datetime.now()
+
         cls._w = nparcel.Writer()
+        cls._dir = tempfile.mkdtemp()
 
     def test_init(self):
         """Initialise a Writer object.
@@ -15,7 +22,61 @@ class TestWriter(unittest2.TestCase):
         msg = 'Object is not an nparcel.Writer'
         self.assertIsInstance(self._w, nparcel.Writer, msg)
 
+    def test_header_write(self):
+        """Write out the headers.
+        """
+        hdrs = ['JOB_ITEM_ID',
+                'JOB_BU_ID',
+                'ji.connote_nbr',
+                'j.card_ref_nbr',
+                'ji.item_nbr',
+                'j.job_ts',
+                'ji.created_ts',
+                'ji.notify_ts',
+                'ji.pickup_ts',
+                'ji.pieces',
+                'ji.consumer_name',
+                'ag.dp_code', 'ag.name']
+        self._w.set_headers(hdrs)
+
+        data = expected = [(20,
+                            1,
+                            'TEST_REF_NOT_PROC',
+                            'aged_parcel_unmatched',
+                            '00393403250082030047',
+                            '%s' % self._now,
+                            '%s' % self._now,
+                            None,
+                            None,
+                            20,
+                            'Con Sumertwenty',
+                            'VIC999',
+                            'VIC Test Newsagent 999'),
+                           (22,
+                            1,
+                            'ARTZ061184',
+                            'JOB_TEST_REF_NOT_PROC_PCKD_UP',
+                            '00393403250082030048',
+                            '%s' % self._now,
+                            '%s' % self._now,
+                            None,
+                            None,
+                            22,
+                            'Con Sumertwentytwo',
+                            'VIC999',
+                            'VIC Test Newsagent 999')]
+
+        file = os.path.join(self._dir, 'test.csv')
+        self._w.set_outfile(file)
+
+        self._w(data)
+
+        # Clean up.
+        remove_files(file)
+
     @classmethod
     def tearDownClass(cls):
         cls._w = None
         del cls._w
+        cls._dir = None
+        del cls._dir
--- nparcel/writer.py
+++ nparcel/writer.py
@@ -1,13 +1,27 @@
 __all__ = [
     "Writer",
 ]
-import nparcel
+import csv
+
+from nparcel.utils.log import log
 
 
 class Writer(object):
     """Toll Parcel Portal Writer class.
+
+    .. attribute:: outfile
+        name of the output file to write to
+
+    .. attribute:: headers
+        name and order of the column headers
+
+    .. attribute:: write_headers
+        write the column names in the first row (default ``True``)
+
     """
     _outfile = None
+    _headers = []
+    _write_headers = True
 
     def __init__(self, outfile=None):
         """Writer initialiser.
@@ -16,9 +30,47 @@ class Writer(object):
         if outfile is not None:
             self._outfile = outfile
 
+    def __call__(self, data):
+        """Class callable that writes list of tuple values in *data*.
+
+        **Args:**
+            *data*: list of tuples to write out
+
+        """
+        log.debug('Preparing "%s" for output' % self.outfile)
+        fh = open(self.outfile, 'wb')
+
+        writer = csv.DictWriter(fh, delimiter=',', fieldnames=self.headers)
+        if self.write_headers:
+            writer.writerow(dict((fn, fn) for fn in self.headers))
+
+        for row in data:
+            writer.writerow(dict(zip(self.headers, row)))
+
+        fh.close()
+
+    @property
     def outfile(self):
         return self._outfile
 
-    @property
     def set_outfile(self, value=None):
         self._outfile = value
+
+    @property
+    def headers(self):
+        return self._headers
+
+    def set_headers(self, values=None):
+        del self._headers[:]
+        self._headers = []
+
+        if values is not None and isinstance(values, list):
+            log.debug('Setting headers to "%s"' % str(values))
+            self._headers.extend(values)
+
+    @property
+    def write_headers(self):
+        return self._headers
+
+    def set_write_headers(self, value=False):
+        self._write_headers = value
