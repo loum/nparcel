9b807955b32251a53fa037126d3ce9f848a99bce
Lou Markovski <lou.markovski@gmail.com>
2014-04-07 20:42:34 +1000

    task/us141-ta121 -- Numeric number generator algorithm
    nparcel/podtranslator.py
    * new method token_generator() that is a Highly customised
      token generator that provides a unique, unambiguous numeric
      representation for a POD identifier

--- nparcel/podtranslator.py
+++ nparcel/podtranslator.py
@@ -2,11 +2,57 @@ __all__ = [
     "PodTranslator",
 ]
 import nparcel
+import time
+
+from nparcel.utils.log import log
 
 
 class PodTranslator(nparcel.Service):
     """:class:`nparcelPodTranslator` object structure.
-    """ 
+    """
 
     def __init__(self):
         nparcel.Service.__init__(self)
+
+    def token_generator(self, dt=None):
+        """Highly customised token generator that provides a unique,
+        unambiguous numeric representation for a POD identifier.
+
+        Seconds since epoch maintains a natural counter.  Token can be
+        forced from a know :class:`datetime.datetime` object via the *dt*
+        parameter.  Otherwise, if *dt* is ``None`` token generation is
+        based on current time.
+
+        **Kwargs:**
+            *dt*: a :class:`datetime.datetime` object to override the token
+            generator against.
+
+        **Returns:**
+            token representation of seconds since epoch that can be used
+            in an unambiguous arrangement with ``job_item.id`` for POD
+            naming
+
+        """
+        token = None
+
+        epoch = 0
+        if dt is not None:
+            # Get the microsecond component.
+            us = dt.microsecond / 1E6
+            epoch = int((time.mktime(dt.timetuple()) + us) * 10)
+        else:
+            # Guarantee, sequential uniqueness.
+            time.sleep(0.5)
+            epoch = int(time.time() * 10)
+
+        if epoch > 0:
+            # Check that it's not above our threshold.
+            if epoch < 20000000000:
+                token = map(int, str(epoch))
+                token[0] = 0
+                token = ''.join(str(x) for x in token)
+            else:
+                log.error('Maximum seconds threshold breached: %d' % epoch)
+
+        log.debug('Token value generated: "%s"' % token)
+        return token
--- nparcel/tests/test_podtranslator.py
+++ nparcel/tests/test_podtranslator.py
@@ -1,4 +1,5 @@
 import unittest2
+import datetime
 
 import nparcel
 
@@ -15,6 +16,38 @@ class TestPodTranslator(unittest2.TestCase):
         msg = 'Object is not an nparcel.PodTranslator'
         self.assertIsInstance(self._pt, nparcel.PodTranslator, msg)
 
+    def test_token_generator_datetime_object(self):
+        """Verify the token_generator -- datetime object provided
+        """
+        dt = datetime.datetime(2014, 04, 07, 19, 22, 00, 123456)
+
+        received = self._pt.token_generator(dt)
+        expected = '03968625201'
+        msg = 'token_generator with datetime object error'
+        self.assertEqual(received, expected, msg)
+
+        dt = datetime.datetime(2033, 05, 18, 13, 33, 19, 999999)
+
+        received = self._pt.token_generator(dt)
+        expected = '09999999999'
+        msg = 'token_generator with datetime object error'
+        self.assertEqual(received, expected, msg)
+
+    def test_token_generator_datetime_object_above_threshold(self):
+        """Verify the token_generator -- datetime object above threshold
+        """
+        dt = datetime.datetime(2033, 05, 18, 13, 33, 20, 1)
+
+        received = self._pt.token_generator(dt)
+        msg = 'token_generator with datetime above threshold error'
+        self.assertIsNone(received, msg)
+
+    def test_token_generator_current_time(self):
+        """Verify the token_generator -- current_time.
+        """
+        received = self._pt.token_generator()
+        msg = 'token_generator using current time'
+        self.assertIsNotNone(received, msg)
 
     @classmethod
     def tearDownClass(cls):
